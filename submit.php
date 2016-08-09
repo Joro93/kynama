36cf8a29f9cc884e64df9d2071f283bfdb076c94
Georgi Yuriev Yordanov <jordanov.apps@gmail.com>
2016-08-09 20:13:44 +0200

    Uploaded site in test, testing contact form and map is now showing

---
+++ submit.php
@@ -0,0 +1,21 @@
+<?php 
+
+session_start();
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = 'From: '.$name.'\n';
+$message .= 'Message: '. $_POST['message'];
+
+$to = "contact@kynama.com";
+
+mail($to, $subject, $message, "From: " . $email);
+
+header('Location: index.html');
+
+
+$_COOKIE['message_sent'] = true;
+
+
+?>
