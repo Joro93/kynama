7fecd9ab28f0b8202e673807a462307ec1e02d7d
Samih Soylu <samih@nivano.nl>
2016-08-10 21:54:41 +0200

    Added a check before actually emailing, so you do not receive a blank email.

--- submit.php
+++ submit.php
@@ -2,17 +2,24 @@
 
 session_start();
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = 'From: '.$name." ";
-$message .= 'Message: '. $_POST['message'];
+if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message'])) {
 
-$to = "contact@kynama.com";
 
-mail($to, $subject, $message, "From: " . $email);
+	# Pointless variables (except message)
+	$name 		= $_POST['name'];
+	$email 		= $_POST['email'];
+	$subject 	= $_POST['subject'];
+	$message 	= 'From: '.$name." ";
+	$message   .= 'Message: '. $_POST['message'];
 
-setcookie("message_sent", true);
+	$to 		= "contact@kynama.com";
+
+	# Mails Georgi and Marto
+	mail($to, $subject, $message, "From: " . $email);
+
+	# Gives me a cookie :3
+	setcookie("message_sent", true);
+}
 
 
 // dont write code below this
